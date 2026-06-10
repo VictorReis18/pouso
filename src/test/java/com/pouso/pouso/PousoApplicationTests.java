cba077f2f7a7aa6a17e3f60514cffd49461cafbc
mthsrnn <mrffreitas@inf.ufpel.edu.br>
2026-06-10 17:09:41 -0300

    feat: initial commit
    Pousamos ^.^

---
+++ src/test/java/com/pouso/pouso/PousoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pouso.pouso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PousoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
